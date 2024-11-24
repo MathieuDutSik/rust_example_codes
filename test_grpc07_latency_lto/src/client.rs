6220a0c5df82961e6ed13d686579460a28a2a5a5
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-11-24 11:50:14 +0100

    Some Addition of more tests.

--- test_grpc07_latency_lto/src/client.rs
+++ test_grpc07_latency_lto/src/client.rs
@@ -103,13 +103,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     //
     {
-        println!("---- Creating the Greeter and iterating over it ----");
+        println!("---- Creating the Greeter from a connected channel and iterating over it ----");
         let endpoint = Endpoint::from_static(endpoint);
 
         let channel = endpoint.connect()
             .await
             .expect("to obtain a channel");
         let mut client = GreeterClient::new(channel);
+        let mut total = Duration::default();
+        for _i in 0..n {
+            let request = tonic::Request::new(HelloRequest {
+                name: "World".into(),
+            });
+
+            let start = Instant::now(); // Start the time
+            let _response = client.say_hello(request).await?;
+            let duration = start.elapsed(); // Measure the elapsed time
+
+            total += duration;
+        }
+        println!("total={:?}", total);
+    }
+    //
+    {
+        println!("---- Creating the Greeter from a lazy connected channel and iterating over it ----");
+        let endpoint = Endpoint::from_static("https://localhost:50052");
+
+        let channel = endpoint.connect_lazy();
+        let mut client = GreeterClient::new(channel);
+        let mut total = Duration::default();
+        for _i in 0..n {
+            let request = tonic::Request::new(HelloRequest {
+                name: "World".into(),
+            });
+
+            let start = Instant::now(); // Start the time
+            let _response = client.say_hello(request).await?;
+            let duration = start.elapsed(); // Measure the elapsed time
+
+            total += duration;
+        }
+        println!("total={:?}", total);
+    }
+    //
+    {
+        println!("---- Creating the Greeter from the Endpoint and iterating over it ----");
+        let endpoint = Endpoint::from_static("https://localhost:50052");
+        let mut client = GreeterClient::connect(endpoint).await.expect("A client");
         let mut total = Duration::default();
         for _i in 0..n {
             let request = tonic::Request::new(HelloRequest {
